2f4b32f7dbe9dfdd4f72470ab340ed0fcca712ca
“lyn <>
2023-11-28 09:50:37 +0800

    完成models

--- app/models/other.py
+++ app/models/other.py
@@ -13,3 +13,13 @@ class Files(Basecls):
     order_id=db.Column(db.Integer, default=0)
     iuser_id=db.Column(db.Integer, default=0)
     cuser_id=db.Column(db.Integer, default=0)
+
+#附件
+class Reports(Basecls):
+    __tablename__ = 'reports'
+
+    title = db.Column(db.Strign(250))
+    iuser_id = db.Column(db.Integer, default=0)
+    filename=db.Column(db.String(200))
+    path=db.Column(db.String(20))
+    type = db.Column(db.String(20))
