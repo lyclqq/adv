33732cc16ee43bd53e19bc2500725ad4917c9c95
“lyn <>
2023-11-30 10:07:29 +0800

    登陆页

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for,redirect,make_response,session,request,flash,send_from_directory,g
+from flask import Flask,render_template,url_for,redirect,make_response,session,request,flash,send_from_directory,g,current_app
 from flask_session import Session
 from flask_wtf.csrf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
@@ -14,6 +14,8 @@ from app.models.other import Reports,Files
 from app.models.bill import Fee1,Fee2,Fee3,Fee4,Fee5,Wordnumbers
 from app.models.system import Users,Logs,Groups
 from app.models.contract import Orders,Customers
+from app.forms.user import LoginForm
+
 app=create_app('develop')
 
 
@@ -59,6 +61,45 @@ def upload():
     f.save(os.path.join(path, newfilename+'.'+extension))
     url = url_for('uploaded_files', filename=datetime.datetime.now().strftime("%Y")+'/'+newfilename+'.'+extension)
     return upload_success(url=url)
+
+@app.route('/login',methods=["GET","POST"])
+def login():
+    form=LoginForm()
+    # 1.判断请求方式,如果是GET,直接渲染页面
+    if request.method == "GET":
+        # 判断管理员是否已经登陆过了,如果登陆过了指教跳转到首页
+        if str(session.get('username')) != 'None':
+            return redirect("/admin/index")
+        return render_template("login.html",form=form)
+    # 2.如果是POST请求,获取参数
+    username = request.form.get("username")
+    password = request.form.get("password")
+    captcha=request.form.get('verify_code')
+    # 3.校验参数,为空校验
+    if not all([username, password]):
+        return render_template("login.html", errmsg="参数不全",form=form)
+    if session.get('imageCode') != captcha:
+        return render_template("login.html", errmsg="验证码不对"+captcha+"*",form=form)
+    # 4.根据用户名取出管理员对象,判断管理员是否存在
+    try:
+        admin = Users.query.filter(Users.username== username).first()
+    except Exception as e:
+        current_app.logger.error(e)
+        return render_template("login.html", errmsg="用户查询失败",form=form)
+    if not admin:
+        return render_template("login.html", errmsg="管理员不存在",form=form)
+
+    # 5.判断管理员的密码是否正确
+    if not admin.check_password(password):
+        return render_template("login.html", errmsg="密码错误",form=form)
+
+    # 6.管理的session信息记录
+    session["user_id"] = admin.id
+    session["username"] = admin.username
+    session["usermenu"]=admin.usermenu
+
+    # 7.重定向到首页展示
+    return redirect("/admin/index")
 
 
 if __name__=='__main__':
