6b9f280bf26b84e98f26ed4b527bffa4a5a20ff7
jxrb <11931735+signalapp@user.noreply.gitee.com>
2023-12-08 16:31:52 +0800

    刊登页面的添加

--- app/view/publish.py
+++ app/view/publish.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, request
 
+from app import db
 from app.models.bill import Fee2
+from app.models.contract import Orders
 
 # 刊登
 publish_bp = Blueprint('publish', __name__)
@@ -8,10 +10,41 @@ publish_bp = Blueprint('publish', __name__)
 
 @publish_bp.route('/publish/list', methods=["GET", "POST"])
 def publish_list():
+    orders = Orders.query.all()
+    ids = ''
+    names = ''
+    for o in orders:
+        names = names + ',' + o.title
+        ids = ids + ',' + str(o.id)
     fee2 = Fee2.query.all()
-    return render_template('publish/publish_list.html', fee2=fee2)
+    return render_template('publish/publish_list.html', fee2=fee2, names=names, ids=ids)
 
 
-@publish_bp.route('/p/i', methods=["GET", "POST"])
-def index():
-    return render_template('index24.html')
+@publish_bp.route('/publish/add', methods=["POST"])
+def publish_add():
+    order_id = request.form.get('order_id')
+    file = request.files.get('filename')
+    f = Fee2(
+    )
+    f.fee = float(request.form.get('fee'))
+    f.order_id = int(order_id)
+    f.area = float(request.form.get('area'))
+    f.feedate = request.form.get('feedate')
+    f.pagename = request.form.get('pagename')
+    f.status = '未审'
+
+    db.session.add(f)
+    db.session.commit()
+    re = '{"result":"ok"}'
+    return re
+
+
+def get_order_list():
+    orders = Orders.query.all()
+    names = []
+    ids = []
+    for o in orders:
+        ids.append(o.id)
+        names.append(o.title)
+    re = '[{"names":' + str(names) + '},{"ids":' + str(ids) + '}]'
+    return re
--- app/view/user.py
+++ app/view/user.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, session, flash, request
+from flask import Blueprint, render_template, session, flash, request, current_app
 from app import db
-from app.common import is_login
+from app.common import is_login, ins_logs
 from app.forms.user import PwdForm
 from app.models.system import Users
 
@@ -29,11 +29,15 @@ def editpwd():
 def edit_pwd():
     pwd2 = request.form.get('new_pwd2')
     if session.get('user_id'):
-        userid = int(session.get('user_id'))
-        user = Users.query.get(userid)
-        user.set_password(pwd2)
-        db.session.add(user)
-        db.session.commit()
+        try:
+            userid = int(session.get('user_id'))
+            user = Users.query.get(userid)
+            user.set_password(pwd2)
+            db.session.add(user)
+            db.session.commit()
+            ins_logs(userid, '修改密码', 'user')
+        except Exception as e:
+            current_app.logger.error(e)
         re = '{"result":"ok"}'
     else:
         re = '{"result":"out_of_date"}'
