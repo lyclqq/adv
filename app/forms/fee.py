1aa1595850b9262603c301836723d779fe261fba
“lyn <>
2023-12-25 15:29:31 +0800

    合同字数

---
+++ app/forms/fee.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from wtforms import StringField,PasswordField,SubmitField,Form,widgets,SelectField,FileField,TextAreaField,DateField,IntegerField,FloatField
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired,Length,NumberRange
+
+
+class WordsForm(FlaskForm):
+    title = StringField(label='合同名称:',
+        widget=widgets.TextInput(),
+        render_kw={'class': 'form-control',
+                   "placeholder":"输入合同名称",'readonly':True}
+    )
+    ordernumber = StringField(label='合同号:',
+        widget=widgets.TextInput(),
+        render_kw={'class': 'form-control','readonly':True}
+    )
+    wordnumber = IntegerField(label='合同字数:', render_kw={'class': 'form-control', 'readonly':True})
+    wordcount = IntegerField(label='己发字数:', render_kw={'class': 'form-control', 'readonly': True})
+    words = IntegerField(label='字数发生额:', validators=[NumberRange(min=0, max=100000, message='字数只能为0-100000')],
+                         render_kw={'class': 'form-control', 'placeholder': '字数只能为0-100000'},default=0)
+
+    notes=TextAreaField(label='备注:',
+        widget=widgets.TextInput(),
+        render_kw={'class': 'form-control',
+                   "placeholder":"字数发生额"})
+    fee_date  = DateField('发生日期', format='%Y-%m-%d',validators=[DataRequired('日期不能为空')],
+                            render_kw={'class': 'form-control','height':'60px'})
+    type=SelectField(label='类型：',render_kw={'class': 'form-control'},
+                           choices=[('order','合同字数' ), ('publish','己发字数' )])
+    submit = SubmitField('提交',render_kw={'class':'btn btn-block btn-info'})
--- app/view/wordsadmin.py
+++ app/view/wordsadmin.py
@@ -8,8 +8,10 @@ from app.common import is_login,ins_logs
 from app import db
 from app.models.contract import Customers,Orders
 from app.models.other import Files
+from app.models.bill import Wordnumbers
 from app.forms.customer import CustomerForm
 from app.forms.order import OrderForm,OrderSearchForm,OrderupfileForm
+from app.forms.fee import WordsForm
 import datetime
 
 wordsadminView=Blueprint('words_admin',__name__)
@@ -31,18 +33,40 @@ def order_search():
     else:
         pagination=orders.search_orders(None,page=page)
     form.status.choices=[('全部','全部'),('己审','己审' ), ('未审','未审' ),( '待审','待审'), ('完成', '完成'),('作废', '作废')]
-    #pagination=orders.query.paginate(page, per_page=current_app.config['PAGEROWS'])
+
     result=pagination.items
     return render_template('wordsadmin/order_search.html', page=page, pagination=pagination, posts=result,form=form)
 
 #字数输入
-@wordsadminView.route('/words_input',methods=["GET","POST"])
+@wordsadminView.route('/words_input/<int:oid>',methods=["GET","POST"])
 @is_login
 def words_input(oid):
-    pass
+    uid = session.get('user_id')
+    page = request.args.get('page', 1, type=int)
+    form=WordsForm()
+    order = Orders.query.filter(Orders.id == oid).first_or_404()
+    form.title.data=order.title
+    form.ordernumber.data=order.ordernumber
+    form.wordnumber.data=order.wordnumber
+    form.wordcount.data=order.wordcount
+    return render_template('wordsadmin/words_input.html', form=form)
 
-#字数查看
-@wordsadminView.route('/words_show',methods=["GET","POST"])
+#己发字数查看
+@wordsadminView.route('/words_show_publish/<int:oid>',methods=["GET","POST"])
 @is_login
-def words_show(oid):
-    pass
+def words_show_publish(oid):
+    uid = session.get('user_id')
+    page = request.args.get('page', 1, type=int)
+    order = Orders.query.filter(Orders.id == oid).first_or_404()
+    pagination=Wordnumbers.query.filter(Wordnumbers.type=='publish').paginate(page, per_page=current_app.config['PAGEROWS'])
+    return render_template('wordsadmin/words_show.html', order=order,pagination=pagination,page=page,title='己发字数查看')
+
+#合同字数查看
+@wordsadminView.route('/words_show_order/<int:oid>',methods=["GET","POST"])
+@is_login
+def words_show_order(oid):
+    uid = session.get('user_id')
+    page = request.args.get('page', 1, type=int)
+    order = Orders.query.filter(Orders.id == oid).first_or_404()
+    pagination=Wordnumbers.query.filter(Wordnumbers.type=='order').paginate(page, per_page=current_app.config['PAGEROWS'])
+    return render_template('wordsadmin/words_show.html', order=order,pagination=pagination,page=page,title='合同字数查看')
