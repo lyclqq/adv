7e58496cf8b8051b194d1c81b36085499bde6817
“lyn <>
2024-01-11 16:05:13 +0800

    完善初始化

--- app/view/user.py
+++ app/view/user.py
@@ -2,9 +2,9 @@ import json
 import os
 from flask import Blueprint, render_template, session, flash, request, current_app
 from app import db
-from app.common import is_login, ins_logs,month_difference,get_SystemMonth
+from app.common import is_login, ins_logs,month_difference
 from app.forms.user import PwdForm,MonthForm
-from app.models.system import Users, Groups
+from app.models.system import Users, Groups,Systeminfo
 from app.models.contract import Orders
 from app.models.other import History
 from sqlalchemy.sql import func
@@ -157,37 +157,51 @@ def reset_pwd():
 @is_login
 def setmonth():
     form = MonthForm()
-    form.today.data=get_SystemMonth()
+    systeminfo = db.session.query(Systeminfo).filter(Systeminfo.id == 1).first()
+    systoday=systeminfo.systemmonth
+    form.today.data=systoday.strftime('%Y-%m')
     if form.validate_on_submit():
-        month=month_difference(form.today.data+'01',form.fee_date.data)
+        month=month_difference(form.today.data+'-01',form.fee_date.data)
         if month==1:
             try:
-                fee21=db.session.query(Orders).with_entities(func.sum(Orders.Fee21)).scalar()
+                fee32=db.session.query(Orders).with_entities(func.sum(Orders.Fee32)).scalar()
                 fee22 = db.session.query(Orders).with_entities(func.sum(Orders.Fee22)).scalar()
+                fee42=db.session.query(Orders).with_entities(func.sum(Orders.Fee42)).scalar()
+                fee52 = db.session.query(Orders).with_entities(func.sum(Orders.Fee52)).scalar()
                 history=History()
-                history.title=form.fee_date.data
-                history.fee_date = form.fee_date.data
+                history.title=form.fee_date.data.strftime('%Y-%m')
+                history.fee_date = form.fee_date.data.strftime('%Y-%m-%d')
                 history.fee=fee22
                 history.type='Fee22'
                 db.session.add(history)
                 history=History()
-                history.title=form.fee_date.data
-                history.fee_date=form.fee_date.data
-                history.fee=fee21
-                history.type='Fee21'
+                history.title=form.fee_date.data.strftime('%Y-%m')
+                history.fee_date=form.fee_date.data.strftime('%Y-%m-%d')
+                history.fee=fee32
+                history.type='Fee32'
                 db.session.add(history)
-                Orders.query.update({'fee22': 0,'fee32':0,'fee42':0,'fee52':0,'fee62':0})
+                history = History()
+                history.title=form.fee_date.data.strftime('%Y-%m')
+                history.fee_date=form.fee_date.data.strftime('%Y-%m-%d')
+                history.fee=fee42
+                history.type='Fee42'
+                db.session.add(history)
+                history = History()
+                history.title=form.fee_date.data.strftime('%Y-%m')
+                history.fee_date=form.fee_date.data.strftime('%Y-%m-%d')
+                history.fee=fee52
+                history.type='Fee52'
+                db.session.add(history)
+                Orders.query.update({'Fee22': 0,'Fee32':0,'Fee42':0,'Fee52':0,'Fee62':0})
+                if form.fee_date.data.year-systoday.year==1:
+                    Orders.query.update({'Fee23': 0, 'Fee33': 0, 'Fee43': 0, 'Fee53': 0})
+                systeminfo.systemmonth= form.fee_date.data.strftime('%Y%m%d')
+                db.session.add(systeminfo)
                 db.session.commit()
-                strpath = os.getcwd() + "\\app\\static\\system.json"
-                with open(strpath, "w") as f1:
-                    sys=json.load(f1)
-                    sys['month']=form.fee_date.data.strftime('%Y%m')
-                with open(strpath, "w") as f2:
-
-                    json.dump(sys, f2)
                 flash('初使化成功!')
             except Exception as e:
                 current_app.logger.error(e)
+                db.session.rollback()
                 flash('初使化失败')
         else:
             flash('所选月份不对!')
