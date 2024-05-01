8e7348852b89067acf0717eb7fb7424b1b74bca9
jxrb <11931735+signalapp@user.noreply.gitee.com>
2024-05-01 19:38:59 +0800

    解决日志查看的问题

--- app/view/logs.py
+++ app/view/logs.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 from app.common import is_login
 from app.models.system import Logs, Users
 
@@ -9,10 +9,13 @@ pagesize = 15
 
 
 # 列表页
-@log_bp.route('/log/list/<int:page>', methods=["GET", "POST"])
+@log_bp.route('/log/list/<int:page>', defaults={"uname": None}, methods=["GET"])
+@log_bp.route('/log/list/<string:uname>/<int:page>', methods=["GET"])
 @is_login
-def log_list(page):
+def log_list(page, uname):
     q = Logs.query
+    if uname:
+        q = q.filter(Logs.user_id == session.get('user_id'))
     pagination = q.order_by(Logs.create_datetime.desc()).paginate(page=page, per_page=pagesize, error_out=False)
     return render_template('log/log_list.html', pagination=pagination)
 
